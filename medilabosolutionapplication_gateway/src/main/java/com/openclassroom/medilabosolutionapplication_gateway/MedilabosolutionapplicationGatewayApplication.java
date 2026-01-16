cefb467adc8121489607cb4ce36df03ed0bdeb05
cleo lachenaud <cleo.lachenaud@gmail.com>
2026-01-16 14:40:19 +0100

    ajout structure minimale gateway + séparation des projets

---
+++ medilabosolutionapplication_gateway/src/main/java/com/openclassroom/medilabosolutionapplication_gateway/MedilabosolutionapplicationGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.openclassroom.medilabosolutionapplication_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedilabosolutionapplicationGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedilabosolutionapplicationGatewayApplication.class, args);
+	}
+
+}
---
+++ medilabosolutionapplication_gateway/src/test/java/com/openclassroom/medilabosolutionapplication_gateway/MedilabosolutionapplicationGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassroom.medilabosolutionapplication_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedilabosolutionapplicationGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
